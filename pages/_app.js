c1fa1c209cd9548d16074326f1b239296ed8ecfa
Elin Ölund Forsling <33157767+ElinOlundForsling@users.noreply.github.com>
2021-02-26 14:21:36 +0100

    restored some values as vercel dont seem to update them

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,9 @@ import Wrapper from '../components/Wrapper';
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  authDomain: 'iku-iku.firebaseapp.com',
+  projectId: 'iku-iku',
+  storageBucket: 'iku-iku.appspot.com',
   messagingSenderId: process.env.FIREBASE_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
 };
